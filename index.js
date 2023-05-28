880eddcacdde989b0f9d803de7def55044288df2
matheustmendes <matios.torrres@gmail.com>
2023-05-28 13:54:15 -0300

    added validations on inputs, w/ regex

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 /* eslint-disable no-use-before-define */
+
+/*
+
+!TODO:
+? procurar formas de fazer validação dos dados antes de criar a arquitetura dos arquivos
+? estudar a melhor forma de leitura e autenticação simples do usuario
+? começar a trabalhar a navegação de cada parte
+?(*seria melhor trabalhar com módulos? criava meio que componentes do codigo em pastas acho que ficaria melhor (estudar isso)*)
+! OBS:
+* Até então só o básico, quando for continuar, se atentar a TODO, mas os inputs basicos estão todos funcionando
+*/
+
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 
@@ -39,25 +51,69 @@ function registerAccount() {
   inquirer.prompt([
     {
       name: 'name',
-      message: 'Type your full name.',
+      message: 'Type your full name.\n →',
+      validate: (input) => {
+        const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+
+        if (!nameRegex.test(input)) {
+          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+          console.log(chalk.redBright('There was an error, exiting...'));
+          process.exit();
+
+          return;
+        }
+        return true;
+      },
 
     },
     {
       name: 'login',
-      message: 'Type your login.',
+      message: 'Type your login.\n→',
+      validate: (input) => {
+        const usernameRegex = /^[^\d\s][\w\s]+$/;
+
+        if (!usernameRegex.test(input)) {
+          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+          console.log(chalk.redBright('There was an error, exiting...'));
+          process.exit();
+        }
+
+        return true;
+      },
     },
 
     {
 
       name: 'email',
-      message: 'Type your email address.',
+      message: 'Type your email address.\n→',
+      validate: (input) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(input)) {
+          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+          console.log(chalk.redBright('There was an error, exiting...'));
+          process.exit();
+        }
+        return true;
+      },
     },
     {
 
       type: 'password',
       mask: true,
       name: 'password',
-      description: 'Type your password.',
+      message: 'Type your password. \nIt must have at least four characters and maximum eight characters.\n→',
+      validate: (input) => {
+        const passwordRegex = /^.{4,8}$/;
+
+        if (!passwordRegex.test(input)) {
+          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+          console.log(chalk.redBright('There was an error, exiting...'));
+          process.exit();
+        }
+
+        return true;
+      },
 
     },
   ]).then((response) => {
