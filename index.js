45789af3fb56c1adba3632604de8711efadde4da
matheustmendes <matios.torrres@gmail.com>
2023-06-04 03:08:34 -0300

    refactor & func refactored index, email validation

--- index.js
+++ index.js
@@ -1,134 +1,137 @@
 /* eslint-disable no-use-before-define */
-
-/*
-
-!TODO:
-? criar arquitetura dos arquivos
-? dar um jeito de percorrer todo diretorio e buscar o email pra ver se já existe
-? estudar a melhor forma de leitura e autenticação simples do usuario
-? começar a trabalhar a navegação de cada parte
-?(*seria melhor trabalhar com módulos? criava meio que componentes do codigo em pastas acho que ficaria melhor (estudar isso)*)
-! OBS:
-* Até então só o básico, quando for continuar, se atentar a TODO, mas os inputs basicos estão todos funcionando
-*/
-
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 import fs from 'fs';
 
-function initialOperation() {
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'actions',
-      message: 'Acess your account.',
-      choices: [
+async function initialOperation() {
+  try {
+    const response = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'actions',
+        message: 'Access your account.',
+        choices: ['Login', 'Register', 'Exit'],
+      },
+    ]);
 
-        'Login',
-        'Register',
-        'Exit',
-      ],
-    },
-  ]).then((response) => {
-    console.log('Everything ok until now..');
+    console.log('Everything is okay until now..');
 
-    createAccount();
-  }).catch((error) => {
+    if (response.actions === 'Register') {
+      createAccount();
+    } else if (response.actions === 'Exit') {
+      console.log(chalk.yellow('Goodbye!'));
+    } else {
+      console.log(chalk.yellow('Login functionality is not implemented yet.'));
+    }
+  } catch (error) {
     console.error(error);
-  });
+  }
 }
+
 initialOperation();
 
-// create account
-
-function createAccount() {
+async function createAccount() {
   console.log(chalk.bgGreen.black('Hi, thanks for creating your account here! We were waiting for you.'));
   console.log(chalk.bgGreen.black('Follow the instructions down below: '));
 
-  registerAccount();
+  await registerAccount();
 }
-// registerAccount
 
-function registerAccount() {
-  inquirer.prompt([
-    {
-      name: 'name',
-      message: 'Type your full name.\n →',
-      validate: (input) => {
-        const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+async function registerAccount() {
+  try {
+    const response = await inquirer.prompt([
+      {
+        name: 'name',
+        message: 'Type your full name.\n →',
+        validate: (input) => {
+          const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
 
-        if (!nameRegex.test(input)) {
-          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
-          console.log(chalk.redBright('There was an error, exiting...'));
-          process.exit();
-
-          return;
-        }
-        return true;
+          if (!nameRegex.test(input)) {
+            console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+            console.log(chalk.redBright('There was an error, exiting...'));
+            process.exit();
+          }
+          return true;
+        },
       },
+      {
+        name: 'login',
+        message: 'Type your login.\n→',
+        validate: (input) => {
+          const usernameRegex = /^[^\d\s][\w\s]+$/;
 
-    },
-    {
-      name: 'login',
-      message: 'Type your login.\n→',
-      validate: (input) => {
-        const usernameRegex = /^[^\d\s][\w\s]+$/;
+          if (!usernameRegex.test(input)) {
+            console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+            console.log(chalk.redBright('There was an error, exiting...'));
+            process.exit();
+          }
 
-        if (!usernameRegex.test(input)) {
-          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
-          console.log(chalk.redBright('There was an error, exiting...'));
-          process.exit();
-        }
-
-        return true;
+          return true;
+        },
       },
-    },
+      {
+        name: 'email',
+        message: 'Type your email address.\n→',
+        validate: (input) => {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    {
-
-      name: 'email',
-      message: 'Type your email address.\n→',
-      validate: (input) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (!emailRegex.test(input)) {
-          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
-          console.log(chalk.redBright('There was an error, exiting...'));
-          process.exit();
-        }
-        return true;
+          if (!emailRegex.test(input)) {
+            console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+            console.log(chalk.redBright('There was an error, exiting...'));
+            process.exit();
+          }
+          return true;
+        },
       },
-    },
-    {
+      {
+        type: 'password',
+        mask: true,
+        name: 'password',
+        message: 'Type your password. \nIt must have at least four characters and maximum eight characters.\n→',
+        validate: (input) => {
+          const passwordRegex = /^.{4,8}$/;
 
-      type: 'password',
-      mask: true,
-      name: 'password',
-      message: 'Type your password. \nIt must have at least four characters and maximum eight characters.\n→',
-      validate: (input) => {
-        const passwordRegex = /^.{4,8}$/;
+          if (!passwordRegex.test(input)) {
+            console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
+            console.log(chalk.redBright('There was an error, exiting...'));
+            process.exit();
+          }
 
-        if (!passwordRegex.test(input)) {
-          console.log(chalk.redBright('\n\nPlease insert the data correctly.'));
-          console.log(chalk.redBright('There was an error, exiting...'));
-          process.exit();
-        }
-
-        return true;
+          return true;
+        },
       },
+    ]);
 
-    },
-  ]).then((response) => {
-    // console.log(response.name, response.login, response.email, response.password);
+    if (await checkEmail(response.email) === false) {
+      const emailData = fs.readFileSync('admin/emails.json', 'utf-8');
+      const emails = JSON.parse(emailData);
+
+      const emailExists = Object.values(emails).includes(response.email);
+
+      if (emailExists) {
+        console.log(chalk.red('The email is already registered. Please, try another.'));
+        process.exit();
+      }
+
+      emails[response.login] = response.email;
+
+      fs.writeFileSync('admin/emails.json', JSON.stringify(emails, null, 2));
+    } else {
+      console.log(chalk.red('The email is already registered. Please, try another.'));
+      process.exit();
+    }
+
+    if (fs.existsSync(`accounts/${response.login}.json`)) {
+      console.log(chalk.redBright('This login is already registered, try another.'));
+      process.exit();
+    }
 
     createArchive(response.login, response.name, response.email, response.password);
-  }).catch((error) => {
+  } catch (error) {
     console.error(error);
     process.exit();
-  });
+  }
 }
-
-// function pra criar o arquivo?
 
 function createArchive(login, username, email, password) {
   const userLogin = login;
@@ -140,14 +143,28 @@ function createArchive(login, username, email, password) {
     fs.mkdirSync('accounts');
   }
 
-  if (fs.existsSync(`accounts/${userLogin}.json`)) {
-    console.log(chalk.redBright('This login is already registered, try another.'));
-    process.exit();
-  }
-
-  fs.writeFileSync(`accounts/${userLogin}.json`, `{\n"name": "${userName}",\n"login": "${userLogin}",\n"email": "${userEmail}",\n"password": "${userPassword}"\n}`);
+  fs.writeFileSync(
+    `accounts/${userLogin}.json`,
+    `{
+  "name": "${userName}",
+  "login": "${userLogin}",
+  "email": "${userEmail}",
+  "password": "${userPassword}"
+}`,
+  );
 
   console.log(chalk.greenBright('Account created. Welcome!'));
 }
 
-// login
+async function checkEmail(email) {
+  try {
+    const emailData = fs.readFileSync('admin/emails.json', 'utf-8');
+    const emails = JSON.parse(emailData);
+    const exists = Object.keys(emails).includes(email);
+
+    return exists;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
